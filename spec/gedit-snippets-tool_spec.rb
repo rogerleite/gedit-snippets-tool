4aa7d77636d831f5e1650e1e08b2b1f6e9df9995
RogerLeite <roger.barreto+github@gmail.com>
2009-04-29 08:08:54 -0300

    Added some simple spec

--- spec/gedit-snippets-tool_spec.rb
+++ spec/gedit-snippets-tool_spec.rb
@@ -1,5 +1,30 @@
-describe "Gedit-snippets-tool" do
-  it "should have real tests" do
-    false.should eql(true)
-  end
+require File.expand_path(File.dirname(__FILE__) + "/../lib/gedit_snippets_tool.rb")
+include GeditSnippetsTool
+
+describe GeditSnippetsTool do
+
+    before do
+        @commands = Commands.new
+    end
+
+    it "should have a version" do
+        VERSION.should_not be_nil
+    end
+
+    it "should respond for -cs argument" do
+        @commands.should_receive(:generate_cheat_sheet)
+        @commands.execute(['-cs'])
+    end
+
+    it "should respond for --cheat-sheet argument" do
+        @commands.should_receive(:generate_cheat_sheet)
+        @commands.execute(['--cheat-sheet'])
+    end
+
+    it "should respond with help for invalid argument" do
+        @commands.should_receive(:show_help)
+        @commands.execute(['-Xnksnksn'])
+    end
+
 end
+
